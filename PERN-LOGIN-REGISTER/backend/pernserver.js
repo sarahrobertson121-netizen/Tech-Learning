ed6eb66c53db1b3e2981f57b231320086f06f627
Sarah Robertson <sarah.robertson121@gmail.com>
2026-03-22 14:50:51 +0000

    pern_server_configuration

--- PERN-LOGIN-REGISTER/backend/pernserver.js
+++ PERN-LOGIN-REGISTER/backend/pernserver.js
@@ -1,11 +1,20 @@
 import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
 
 const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
