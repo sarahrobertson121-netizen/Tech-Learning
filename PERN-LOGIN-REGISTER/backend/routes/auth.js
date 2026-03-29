4cdd5bec17473bb179828f935d8602ca40c1417c
Sarah Robertson <sarah.robertson121@gmail.com>
2026-03-29 15:48:14 +0100

    protect routes

--- PERN-LOGIN-REGISTER/backend/routes/auth.js
+++ PERN-LOGIN-REGISTER/backend/routes/auth.js
@@ -1,92 +1,105 @@
-import express from 'express';
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
-import pool from '../config/db.js';
+import express from "express";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import pool from "../config/db.js";
 
 const router = express.Router();
 
 const cookieOptions = {
   httpOnly: true,
-  secure: process.env.NODE_ENV === 'production',
-  sameSite: 'Strict',
-  maxAge: 30 * 24 * 60 * 60 * 1000 // 30 days
-}
+  secure: process.env.NODE_ENV === "production",
+  sameSite: "Strict",
+  maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+};
 
 const generateToken = (id) => {
-  return jwt.sign({id}, process.env.JWT_SECRET, {
-    expiresIn: '30d'
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
   });
-}
+};
 
 //Register
 
-router.post('/register', async (req, res) => {
-  const { username, email, password} = req.body;
+router.post("/register", async (req, res) => {
+  const { username, email, password } = req.body;
 
   if (!username || !email || !password) {
-    return res.status(400).json({ message:'Please provide all required fields'})
+    return res
+      .status(400)
+      .json({ message: "Please provide all required fields" });
   }
 
-  const userExists = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+  const userExists = await pool.query("SELECT * FROM users WHERE email = $1", [
+    email,
+  ]);
 
   if (userExists.rows.length > 0) {
-    return res.status(400).json({ message: 'User already exists' });
+    return res.status(400).json({ message: "User already exists" });
   }
 
   const hashedPassword = await bcrypt.hash(password, 10);
 
   const newUser = await pool.query(
-    'INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING id, name, email', 
-    [username, email,hashedPassword]
+    "INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING id, name, email",
+    [username, email, hashedPassword],
   );
 
   const token = generateToken(newUser.rows[0].id);
 
-  res.cookie('token', token, cookieOptions);
+  res.cookie("token", token, cookieOptions);
 
   return res.status(201).json({ user: newUser.rows[0] });
-})
+});
 
 // Login
-router.post('/login', async (req, res) => {
+router.post("/login", async (req, res) => {
   const { email, password } = req.body;
 
   if (!email || !password) {
-    return res.status(400).json({ message: 'Please provide all required fields'})
+    return res
+      .status(400)
+      .json({ message: "Please provide all required fields" });
   }
 
-  const user = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+  const user = await pool.query("SELECT * FROM users WHERE email = $1", [
+    email,
+  ]);
 
   if (user.rows.length === 0) {
-    return res.status(400),json({ message: 'Invalid credentials' });
+    return (res.status(400), json({ message: "Invalid credentials" }));
   }
 
   const userData = user.rows[0];
 
   const isMatch = await bcrypt.compare(password, userData.password);
 
-  if(!isMatch) {
-    return res.status(400).json({message: 'Invalid credentials'});
+  if (!isMatch) {
+    return res.status(400).json({ message: "Invalid credentials" });
   }
 
   const token = generateToken(userData.id);
 
-  res.cookie('token', token, cookieOptions);
+  res.cookie("token", token, cookieOptions);
 
-  res.json({ user: { id: userData.id, username: userData.username, email: userData.email } });
-})
+  res.json({
+    user: {
+      id: userData.id,
+      username: userData.username,
+      email: userData.email,
+    },
+  });
+});
 
 //Me
-router.get('/me', async (req, res) =>{
-  res.json(req.user)
-//return info of the logged in user from protect middleware
-})
+router.get("/me", protect, async (req, res) => {
+  res.json(req.user);
+  //return info of the logged in user from protect middleware
+});
 
 //Logout
-router.post('/logout', (req, res) => {
-  res.cookie('token', '', {...cookieOptions, maxAge: 1});
-  res.json({ message: 'Logged out successfully'});
-})
+router.post("/logout", (req, res) => {
+  res.cookie("token", "", { ...cookieOptions, maxAge: 1 });
+  res.json({ message: "Logged out successfully" });
+});
 
 export default router;
-
