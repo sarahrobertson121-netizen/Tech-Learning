6fa13b4036c8f8fc5cc2af8ca902c55380031b72
Sarah Robertson <sarah.robertson121@gmail.com>
2026-03-29 15:47:17 +0100

    middleware config

--- PERN-LOGIN-REGISTER/backend/middleware/authmiddleware.js
+++ PERN-LOGIN-REGISTER/backend/middleware/authmiddleware.js
@@ -1,1 +1,31 @@
-import 
+import jwt from "jsonwebtoken";
+import pool from "../config/db.js";
+
+export const protect = async (req, res, next) => {
+  try {
+    const token = req.cookies.token;
+
+    if (!token) {
+      return res.status(401).json({ message: "Not authorised, no token" });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await pool.query(
+      "SELECT id, username, email FROM users WHERE id = £1",
+      [decoded.id],
+    );
+
+    if (user.rows.length === 0) {
+      return res
+        .status(401)
+        .json({ message: "Not authorised, user not found" });
+    }
+
+    req.user = user.rows[0];
+    next();
+  } catch (error) {
+    console.error(error);
+    res.status(401).json({ message: "Not authorised, token failed" });
+  }
+};
